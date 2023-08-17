89e6eff36e23aabf14f8eb6710c8cc4f17fc541f
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-17 16:13:59 +0530

    Update Build.groovy

--- src/com/packages/Build.groovy
+++ src/com/packages/Build.groovy
@@ -1,1 +1,11 @@
+def setBuildInfo(env, branch, module) {
+    wrap([$class: 'BuildUser']) {
+        script {
+            def changeLogSets = currentBuild.changeSets
+            currentBuild.displayName = "#${env}-#${branch}-#${currentBuild.number}"
+            currentBuild.description = "Module: ${module} \n Build By: ${BUILD_USER}"
+        }
+    }
+}
+
 
