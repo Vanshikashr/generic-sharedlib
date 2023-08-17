12854263427e1551199a0f4a80623e12fe60608e
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-17 16:50:22 +0530

    Update Deploy.groovy

--- src/com/packages/Deploy.groovy
+++ src/com/packages/Deploy.groovy
@@ -1,1 +1,8 @@
+// deploy
+def deployApp(String helmBranch, String helmRepo, String env, String module, String dockerImageTag, String kubeConfig) {
+    script {
+        git branch: "${helmBranch}", url: "${helmRepo}", changelog: true, poll: true
+        helmInstall(env, "${env}-${module}", module, dockerImageTag, kubeConfig)
+    }
+}
 
