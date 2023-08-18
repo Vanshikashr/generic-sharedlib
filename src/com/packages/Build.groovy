538a05503a949a9d542329d911f5d668bd6c4345
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-18 09:32:42 +0530

    Update Build.groovy

--- src/com/packages/Build.groovy
+++ src/com/packages/Build.groovy
@@ -1,23 +1,23 @@
 // build done by whom and id and all
 def setBuildInfo(String env, String branch, String module) {
     wrap([$class: 'BuildUser']) {
-        script {
+      
             def changeLogSets = currentBuild.changeSets
             currentBuild.displayName = "#${env}-#${branch}-#${currentBuild.number}"
             currentBuild.description = "Module: ${module} \n Build By: ${BUILD_USER}"
-        }
+        
     }
 }
 
 // build artifact
 def buildArtifacts(String workspace, String s3BucketName, String s3BucketPath, String regionName) {
-    script {
+    
         sh """#!/bin/bash
             set -xe
             echo $workspace
             aws s3 cp s3://${s3BucketName}/${s3BucketPath} . --recursive --region ${regionName}
         """
-    }
+    
 }
 
 
