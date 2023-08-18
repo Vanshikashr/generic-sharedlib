f65a506f8a352a3a9411cc48f5869ebe374c3e68
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-18 14:06:09 +0530

    Update Deploy.groovy

--- src/com/packages/Deploy.groovy
+++ src/com/packages/Deploy.groovy
@@ -1,8 +1,8 @@
 // deploy
-def deployApp() {
+def deployApp(String HELM_BRANCH, String HELM_REPO, String ENV, String MODULE , String DOCKER_IMAGE_TAG, String kubeConfig) {
 
         git branch: "${HELM_BRANCH}", url: "${HELM_REPO}", changelog: true, poll: true
-        helmInstall("${ENV}, "${ENV}-${MODULE}", MODULE, KUBE_CONFIG )
+        helmInstall("${ENV}, "${ENV}-${MODULE}", MODULE, DOCKER_IMAGE_TAG, KUBE_CONFIG )
     
 }
 
