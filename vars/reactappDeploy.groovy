d48450e8c382d888c2bd3004521823e1968c72a8
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-18 14:53:35 +0530

    Update reactappDeploy.groovy

--- vars/reactappDeploy.groovy
+++ vars/reactappDeploy.groovy
@@ -2,7 +2,7 @@
 
 import com.packages.Utilities
 import com.packages.Build
-#import com.packages.Deploy
+//import com.packages.Deploy
 
 def call( ENV ,  BRANCH_NAME, MODULE,  DEFAULT_PROJECT_PREFIX,  REGION_NAME, REPOSITORY_NUMBER,  GIT_URL,  S3_BUCKET_NAME, S3_BUCKET_PATH,  HELM_REPO, HELM_BRANCH,  KUBE_CONFIG)
 {
