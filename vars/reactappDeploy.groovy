fdafa8eb2233d3608228695726717281e1371ec0
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-21 09:20:05 +0530

    Update reactappDeploy.groovy

--- vars/reactappDeploy.groovy
+++ vars/reactappDeploy.groovy
@@ -2,7 +2,7 @@
 
 import com.packages.Utilities
 import com.packages.Build
-//import com.packages.Deploy
+import com.packages.Deploy
 
 def call( ENV ,  BRANCH_NAME, MODULE,  DEFAULT_PROJECT_PREFIX,  REGION_NAME, REPOSITORY_NUMBER,  GIT_URL,  S3_BUCKET_NAME, S3_BUCKET_PATH,  HELM_REPO, HELM_BRANCH,  KUBE_CONFIG)
 {
@@ -58,10 +58,8 @@ pipeline {
             steps {
 
                 script {
-                utilities.dockerImagePush(
-                    env.REGION_NAME,
-                    env.REPOSITORY_NUMBER
-                )
+                utilities.dockerImagePush(REGION_NAME,REPOSITORY_NUMBER)
+                   
                 }
             }
         }
