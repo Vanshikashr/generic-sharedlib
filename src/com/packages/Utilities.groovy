051caa3876f70dfcfae936ab4bd21ae596d65f55
Vanshikashr <130561304+Vanshikashr@users.noreply.github.com>
2023-08-22 14:32:25 +0530

    Update Utilities.groovy

--- src/com/packages/Utilities.groovy
+++ src/com/packages/Utilities.groovy
@@ -90,13 +90,13 @@ def dockerImagePush(REGION_NAME, REPOSITORY_NUMBER, DOCKER_IMAGE_NAME) {
 
         echo "Docker Image Push"
         aws ecr get-login-password --region ${REGION_NAME} | docker login --username AWS --password-stdin ${REPOSITORY_NUMBER}.dkr.ecr.${REGION_NAME}.amazonaws.com
-        docker rmi -f ${setupEnvironments.DOCKER_IMAGE_NAME}
-        docker buildx build --platform linux/arm64 --provenance=false -f Dockerfile --build-arg artifact_version=${COMMIT_ID} -t ${setupEnvironments.DOCKER_IMAGE_NAME} -t ${ECS_IMAGE_NAME} --push .
+        docker rmi -f ${DOCKER_IMAGE_NAME}
+        docker buildx build --platform linux/arm64 --provenance=false -f Dockerfile --build-arg artifact_version=${COMMIT_ID} -t ${DOCKER_IMAGE_NAME} -t ${ECS_IMAGE_NAME} --push .
 
         if [ \$? -eq 0 ]
         then
             echo "Successfully image tagged and pushed to repository"
-            echo ${setupEnvironments.DOCKER_IMAGE_NAME} > $WORKSPACE/image_id
+            echo ${DOCKER_IMAGE_NAME} > $WORKSPACE/image_id
             cat $WORKSPACE/image_id
         else
             echo "Error in tagging/pushing image"
